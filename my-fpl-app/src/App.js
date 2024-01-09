9ed97fe4bbc99b86165847b30a623a8234bbd748
CarlosGenis <carlosgenis06@gmail.com>
2024-01-08 22:05:56 -0500

    Created match pages

--- my-fpl-app/src/App.js
+++ my-fpl-app/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Match from './match.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Fantasy Premier League</h1>
+    </>
   );
 }
 
